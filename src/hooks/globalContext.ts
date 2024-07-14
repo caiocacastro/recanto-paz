0391fadb9e1737621304ddcf275fedbb8f54c141
Caio Castro <Caio@Caios-Macbook-Pro.local>
2024-07-14 13:07:36 -0300

    adicionado todos

--- src/hooks/globalContext.ts
+++ src/hooks/globalContext.ts
@@ -1,9 +1,11 @@
-import { createContext, useContext } from "react";
-import { User } from "../interfaces/User";
+import { createContext, useContext } from 'react';
+import { User } from '../interfaces/User';
 
 export type GlobalContent = {
   logged?: User;
   setLogged: (user?: User) => void;
+  // TODO : Adicionar tabela de usuários no contexto.
+  // TODO : Adicionar mensagens de erro em array no contexto do sistema
 };
 
 export const MyGlobalContext = createContext<GlobalContent>({
